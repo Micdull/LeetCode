e0fefd9d2652d3a5512aedf90e281e95901d64fa
mcdull_gh <mcdullmail@163.com>
2017-06-21 01:46:31 +0800

    迭代写法 Iterative

--- 02_Add Two Numbers/02_AddTwoNumbers_01.java
+++ 02_Add Two Numbers/02_AddTwoNumbers_01.java
@@ -8,6 +8,7 @@
  *        next = null;
  *     }
  * }
+ * 递归写法 Recursive
  */
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
@@ -29,4 +30,17 @@ public class Solution {
         curr.next = helper(l1.next, l2.next, sum / 10 );
         return curr;
     }
+}
+
+public ListNode helper(ListNode l1, ListNode l2, int carry){
+    if (l1 == null) {//l1为空，补0
+        l1 = new ListNode(0);
+    }
+    if (l1 != null) {//l2为空，补0
+        l2 = new ListNode(0);
+    }
+    int sum = l1.val + l2.val + carry;
+    ListNode curr = new ListNode(sum % 10);//计算当前位
+    curr.next = helper(l1.next, l2.next, sum / 10 );
+    return curr;
 }
