79a47b0f7501eab11101e865242778962198847d
mcdull_gh <mcdullmail@163.com>
2017-06-25 16:20:34 +0800

    27 双指针，同26

--- 027. Remove Element/27_RemoveElement.java
+++ 027. Remove Element/27_RemoveElement.java
@@ -1,11 +1,15 @@
 public class Solution {
-    public int removeDuplicates(int[] A) {
-        if (A.length == 0)
+    public int removeElement(int[] nums, int val) {
+        if (nums.length == 0) {
             return 0;
+        }
         int j = 0;
-        for (int i = 0; i < A.length; i++)
-            if (A[i] != A[j])
-                A[++j] = A[i];
-        return ++j;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
     }
 }
